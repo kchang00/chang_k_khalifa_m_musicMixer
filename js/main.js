5276b2dcfc0f77b50c007772f3bb75f94fa1585c
Khalifa <m_khalifa@tss-hr114-dm2.fc.ca>
2019-03-06 14:32:52 -0500

    drag and drop working

--- js/main.js
+++ js/main.js
@@ -1,37 +1,47 @@
-
-console.log("Javascript is linked up");
-
-// load page
-// reset game
-// make icons draggable
-
-// For specific icon :
-
-// if touching game area box then
-// 	make character appear
-// 	play audio until done 
-
-// Reset button reloads page
-
-//-------------------------------------//
-
 (() => {
- 
-// create variable to store audio files
- 	let allAudio = document.querySelectorAll(".audio");
 
-//override default to enable drop
+let images = document.querySelectorAll('.image');
+	dropZones = document.querySelectorAll('.dropZone');
+//console.log(dropZones);
+
+initDrag();
+
+function initDrag() {
+			images.forEach(image => {
+				image.addEventListener('dragstart', function(e) {
+					console.log('draggin...');
+					e.dataTransfer.setData('text/plain', this.id);
+				});
+			});
+		}
+
+	//handle drag over and drop
+	dropZones.forEach(zone => {
+			zone.addEventListener("dragover", function(e) {
+				e.preventDefault();
+				console.log("you dragged over me!");
+			});
+
+			
+				zone.addEventListener("drop", function(e) {
+					if (zone.firstChild == null) {
+						e.preventDefault();
+						console.log("you dropped sumpin on me");
+
+						let img = e.dataTransfer.getData('text/plain');
+						//debugger;
+						e.target.appendChild(document.querySelector(`#${img}`));
+						
+					}
+					else {return;}
+				});
+			
+	});
 
 
-//create drag function
 
-//create drop function
-
-//create audio play function 
-
-
-//create event to trigger audio to play once droppped in the 'play music' box
-
-// Reset button reloads page
 
 })();
+ 
+
+
